91aaa344f2985fb0879129cfdd6ebf3d761b23e9
rlicata34 <rlicata34@gmail.com>
2025-03-05 16:43:14 -0500

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -10,7 +10,7 @@ const validateURL = (value, helpers) => {
 
 module.exports.validateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
+    username: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
@@ -47,7 +47,7 @@ module.exports.validateLogin = celebrate({
 
 module.exports.validateUpdateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
+    username: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
